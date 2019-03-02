d12e85b60fad532cbbdedb2ae7e2f89c6a75181d
Tatiana Quintana <tatsqui@gmail.com>
2019-03-01 16:41:46 -0800

    "created Robot class, added Rakefile"

--- 02-robot-name/robot_name.rb
+++ 02-robot-name/robot_name.rb
@@ -1,1 +1,23 @@
+require 'awesome_print'
+require 'colorize'
+
+class Robot
+  attr_reader :name
+
+  def initialize
+    letters = ("A".."Z").to_a.sample(2) 
+    numbers = (0..9).to_a.sample(3)
+    @name = (letters + numbers).join 
+  end
+
+  def reset
+    letters = ("A".."Z").to_a.sample(2) 
+    numbers = (0..9).to_a.sample(3)
+    @name = (letters + numbers).join 
+    return @name 
+  end
+end
+
+
+
 
