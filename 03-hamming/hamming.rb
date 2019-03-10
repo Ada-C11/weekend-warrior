ab8f24b4f0dfda4497018d3b28ae70d028c39fa5
tatsqui <34796138+tatsqui@users.noreply.github.com>
2019-03-09 21:23:50 -0800

    Update hamming.rb

--- 03-hamming/hamming.rb
+++ 03-hamming/hamming.rb
@@ -1,13 +1,15 @@
-def hamming(string1, string2)
-  hamming_distance = 0
-  string1.length.times do |i|
-    string1[i] == string2[i] ? next : hamming_distance += 1
-    i += 1
+class Hamming
+  attr_reader :compute
+
+  def self.compute(string1, string2)
+    hamming_distance = 0
+    string1.length == string2.length ? true : raise ArgumentError
+    string1.length.times do |i|
+      string1[i] == string2[i] ? next : hamming_distance += 1
+      i += 1
+    end
+    return hamming_distance
   end
-  return hamming_distance
+
+
 end
-
-string_1 =   "GAGCCTACTAACGGGAT"
-string_2 =   "CATCGTAATGACGGCCT"
-
-puts hamming(string_1, string_2)
