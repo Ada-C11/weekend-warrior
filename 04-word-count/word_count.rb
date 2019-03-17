6167ac4bfb66cacd740830eace5c8e3cd25e2011
Tatiana Quintana <tatsqui@gmail.com>
2019-03-16 23:38:46 -0700

    added initialize method and began method to convert to hash

--- 04-word-count/word_count.rb
+++ 04-word-count/word_count.rb
@@ -1,1 +1,14 @@
+class WordCount
+  def initialize(word)
+    @word = hashed_word(word)
+  end
 
+  def words(word)
+    words_array = word.split(" ")
+    hashify = Hash.new(0)
+
+    words_array.each { |word| result[word] += 1}
+    return hashify
+  end
+
+end
